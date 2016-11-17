e20c8aeb754c53b8ef1e2f6041af309137d38c96
Andrew Findlay <andrewrfindlay@ntlworld.com>
2016-11-17 08:45:34 +0000

    Created file for all standard note frequencies and made class spit out c major scale

--- NotesGeneration/main.py
+++ NotesGeneration/main.py
@@ -1,12 +1,4 @@
-import wave, struct, math, cmath, random, notes
+from notes import *
 
-
-sampleLength = 44100*2
-frequency = 261.6
-sampleRate = float(44100)
-volume = 1
-bitDepth = 32767
-
-noteParams = [frequency, sampleRate, sampleLength, bitDepth, volume]
-
-middleC = notes.Note(noteParams[0], noteParams[1], noteParams[2], noteParams[3], noteParams[4])
+for note in cMajor:
+    Note(note, sampleRate, sampleLength, bitDepth, volume)
--- NotesGeneration/notes.py
+++ NotesGeneration/notes.py
@@ -1,11 +1,18 @@
-import wave, struct, math, cmath, random
+import wave, struct, math
+from noteFrequencies import *
 
+sampleLength = 44100
+sampleRate = float(44100)
+volume = 1
+bitDepth = 32767
+
+cMajor = [C4, CS4, D4, DS4, E4, F4, FS4, G4, GS4, A4, AS4, B4, C5]
 
 class Note:
 
-    def __init__(self, frequency, samplerate, samplelength, bitdepth, volume):
+    def __init__(self, (name, frequency), samplerate, samplelength, bitdepth, volume):
         self.frequency = frequency
-        self.note = wave.open('sound.wav', 'w')
+        self.note = wave.open(str(name) + '_Note.wav', 'w')
         self.note.setparams((2, 2, bitdepth, samplelength, 'NONE', 'not compressed'))
 
         for i in range(0, samplelength):
