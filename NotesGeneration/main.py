534177c762d896775eac10527b9aaddda62fe51e
Andrew Findlay <andrewrfindlay@ntlworld.com>
2016-11-17 09:19:13 +0000

    added filepath function

--- NotesGeneration/main.py
+++ NotesGeneration/main.py
@@ -1,11 +1,13 @@
 from notes import *
 
+def filepath(note):
+    return str('Notes/' + str(note) + '_Note.wav')
+
 # this will generate all the notes so only needs to run the first time
-for item in cMajor:
-    Note(item, sampleRate, sampleLength, bitDepth, volume)
+# for item in cMajor:
+#     Note(item, sampleRate, sampleLength, bitDepth, volume)
 
 # plays through cMajor
 for item in cMajor:
     name, frequency = item
-    filePath = str('Notes/' + str(name) + '_Note.wav')
-    winsound.PlaySound(filePath, winsound.SND_FILENAME)
+    winsound.PlaySound(filepath(name), winsound.SND_FILENAME)
