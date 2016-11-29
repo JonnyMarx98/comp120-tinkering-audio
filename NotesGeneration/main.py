060fa5203a7908c2ffdce535f74f7efc3175f5fd
newtoto <andrewrfindlay@ntlworld.com>
2016-11-29 13:27:10 +0000

    more procedural stuff

--- NotesGeneration/main.py
+++ NotesGeneration/main.py
@@ -1,20 +1,54 @@
 from notes import *
-import winsound
+import winsound, pygame
 
 # function to play note (takes string for note name)
-def play(note):
-    winsound.PlaySound(str('Notes/' + str(note) + '.wav'), winsound.SND_FILENAME)
+def play(note, identifyer):
+    winsound.PlaySound(str('Notes/' + str(note) + '_' + identifyer + '.wav'), winsound.SND_FILENAME)
 
 # this will generate all the notes so only needs to run the first time
 
 # Note(RandomNote, sampleRate, sampleLength, bitDepth, volume)
 #
 # play(RandomNote)
+melody = []
+chorus = []
+# set this to procedurally generate rhythm
+# total bar length = 2, split up randomly taking 1/2, 1/4, 1/8, 1/16, 1/32 and append to list
+# make sure it doesn't go over minus
+# randomBeat = random.randint(2, 4, 8, 16, 32)
+# print 1/randomBeat
+notelength = [4, 2, 4, 4, 2, 2, 16, 16, 8, 4, 4, 8, 4, 2, 4, 4, 4, 8, 8]
 
-noteInput = notegen(), notes[notegen()]
+for i in xrange(len(notelength)):
+    samplelength = 44100/(notelength[i])
+    noteChoice = notegen()
+    noteInput = (noteChoice, notes[noteChoice])
+    print noteInput
+    Note(noteInput, sampleRate, samplelength, bitDepth, volume, ('melody' + str(i)))
+    melody.append(noteChoice)
 
-print noteInput
+for i in xrange(8):
+    samplelength = 44100 / 16
+    noteChoice = notegen()
+    noteInput = (noteChoice, notes[noteChoice])
+    print noteInput
+    Note(noteInput, sampleRate, samplelength, bitDepth, volume, 'chorus')
+    chorus.append(noteChoice)
 
-Note(noteInput, sampleRate, sampleLength, bitDepth, volume)
+while True:
 
-print noteInput[0]
+    for i in xrange(2):
+        print 'melody'
+        j = 0
+        for pitch in melody:
+            play(pitch, ('melody' + str(j)))
+            j += 1
+        j = 0
+
+    for i in xrange(2):
+        for times in xrange(3):
+            print 'chorus'
+            for pitch in chorus:
+                print times
+                for i in xrange(times):
+                    play(pitch, 'chorus')
--- NotesGeneration/notes.py
+++ NotesGeneration/notes.py
@@ -1,19 +1,20 @@
 import wave, struct, math, random
 from noteFrequencies import *
 
-sampleLength = 44100/2
+sampleLength = 44100/16
 sampleRate = float(44100)
 volume = 1
 bitDepth = 32767
 
 notePrefix = ['C', 'CS', 'D', 'DS', 'E', 'F', 'FS', 'G', 'GS', 'A', 'AS', 'B']
-
+fSharpMinor = ['FS', 'GS', 'A', 'B', 'CS', 'D', 'E']
+CMajor = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 
 def notegen():
     prefixChoice = random.randint(0, 11)
-    suffixChoice = random.randint(0, 8)
+    suffixChoice = random.randint(0, 1)
 
-    return notePrefix[prefixChoice] + str(suffixChoice)
+    return notePrefix[prefixChoice] + str(4)
 
 # denotes which notes are loaded and saved
 #notes = [C4, CS4, D4, DS4, E4, F4, FS4, G4, GS4, A4, AS4, B4, C5]
@@ -23,9 +24,9 @@ def notegen():
 
 class Note:
 
-    def __init__(self, (name, frequency), samplerate, samplelength, bitdepth, volume):
+    def __init__(self, (name, frequency), samplerate, samplelength, bitdepth, volume, identifyer):
         self.frequency = frequency
-        self.note = wave.open('Notes/' + str(name) + '.wav', 'w')
+        self.note = wave.open('Notes/' + str(name) + '_' + identifyer + '.wav', 'w')
         self.note.setparams((2, 2, bitdepth, samplelength, 'NONE', 'not compressed'))
 
         for i in range(0, samplelength):
